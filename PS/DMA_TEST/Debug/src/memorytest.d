6ebb7a1db5c05193a77de5ca7c80a11e5bccbe4e
Seradory <aliserdar.yurtcan@hotmail.com>
2023-12-08 08:35:32 +0300

    v1
    DMA Üzerinden DDR Veri yazma ve okuma dizayn temelleri uygulandı. Scatter Gather mode ve MicroDMA yapıları incelendi. Simple Mode üzerinde deneme yapıldı.

---
+++ PS/DMA_TEST/Debug/src/memorytest.d
@@ -0,0 +1,35 @@
+src/memorytest.o src/memorytest.o: ../src/memorytest.c \
+ C:/Users/alise/Desktop/VIVADO/PROJECTS/NEXYS_DDR_DMA_LOOP/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/xparameters.h \
+ C:/Users/alise/Desktop/VIVADO/PROJECTS/NEXYS_DDR_DMA_LOOP/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/xil_types.h \
+ C:/Users/alise/Desktop/VIVADO/PROJECTS/NEXYS_DDR_DMA_LOOP/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/xstatus.h \
+ C:/Users/alise/Desktop/VIVADO/PROJECTS/NEXYS_DDR_DMA_LOOP/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/xil_types.h \
+ C:/Users/alise/Desktop/VIVADO/PROJECTS/NEXYS_DDR_DMA_LOOP/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/xil_assert.h \
+ C:/Users/alise/Desktop/VIVADO/PROJECTS/NEXYS_DDR_DMA_LOOP/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/xil_testmem.h \
+ ../src/platform.h ../src/platform_config.h ../src/memory_config.h \
+ C:/Users/alise/Desktop/VIVADO/PROJECTS/NEXYS_DDR_DMA_LOOP/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/xil_printf.h \
+ C:/Users/alise/Desktop/VIVADO/PROJECTS/NEXYS_DDR_DMA_LOOP/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/xparameters.h \
+ C:/Users/alise/Desktop/VIVADO/PROJECTS/NEXYS_DDR_DMA_LOOP/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/bspconfig.h
+
+C:/Users/alise/Desktop/VIVADO/PROJECTS/NEXYS_DDR_DMA_LOOP/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/xparameters.h:
+
+C:/Users/alise/Desktop/VIVADO/PROJECTS/NEXYS_DDR_DMA_LOOP/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/xil_types.h:
+
+C:/Users/alise/Desktop/VIVADO/PROJECTS/NEXYS_DDR_DMA_LOOP/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/xstatus.h:
+
+C:/Users/alise/Desktop/VIVADO/PROJECTS/NEXYS_DDR_DMA_LOOP/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/xil_types.h:
+
+C:/Users/alise/Desktop/VIVADO/PROJECTS/NEXYS_DDR_DMA_LOOP/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/xil_assert.h:
+
+C:/Users/alise/Desktop/VIVADO/PROJECTS/NEXYS_DDR_DMA_LOOP/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/xil_testmem.h:
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../src/memory_config.h:
+
+C:/Users/alise/Desktop/VIVADO/PROJECTS/NEXYS_DDR_DMA_LOOP/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/xil_printf.h:
+
+C:/Users/alise/Desktop/VIVADO/PROJECTS/NEXYS_DDR_DMA_LOOP/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/xparameters.h:
+
+C:/Users/alise/Desktop/VIVADO/PROJECTS/NEXYS_DDR_DMA_LOOP/PS/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_microblaze_0/bspinclude/include/bspconfig.h:
